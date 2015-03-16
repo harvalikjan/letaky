8b8a57063b15ebe8bddc7fcb66d261c45d24d29d
harvalikjan <harvalikjan@gmail.com>
2015-03-16 18:51:32 +0100

    Update list.js

--- list.js
+++ list.js
@@ -1,273 +1,107 @@
+function init() {
+    // toBuy list 
+	// Parse local storage toBuy list to array 
+	var toBuy = JSON.parse(localStorage["toBuy"]);
 
-<!DOCTYPE html><html class=''>
-<head>
-<meta charset='UTF-8'><meta name="robots" content="noindex">
-<link rel="canonical" href="http://codepen.io/dimsemenov/pen/ZYbPJM" />
-<link rel="stylesheet" type="text/css" href="http://192.168.1.101/c/main.css" />
-<link rel='stylesheet prefetch' href='http://photoswipe.s3.amazonaws.com/pswp/dist/photoswipe.css'>
-<link rel='stylesheet prefetch' href='http://photoswipe.s3.amazonaws.com/pswp/dist/default-skin/default-skin.css'>
-<style class="cp-pen-styles">.my-simple-gallery {
-  width: 100%;
-  float: left;
+	// for each of toBuy list   
+	toBuy.forEach(function(listEntries) {
+
+	   var stringifiedLine = JSON.stringify(listEntries);
+	   var elementsOfLines = JSON.parse(stringifiedLine);
+
+	   // line from addListItem()
+	   item = $('<li class="item"><button class="checkbox">&#x2713;</button><span class="list">' + elementsOfLines + '</span><button class="delete">X</button></li>');
+	   $('#itemList').append(item);   
+	});
+
+    // Bought list 
+	// Parse local storage bought list to array 
+	var bought = JSON.parse(localStorage["bought"]);
+
+	// for each of bought list   
+	bought.forEach(function(listEntries) {
+
+	   var stringifiedLine = JSON.stringify(listEntries);
+	   var elementsOfLines = JSON.parse(stringifiedLine);
+	   
+	   // line from addListItem()
+	   item = $('<li class="item bought"><button class="checkbox">&#x2713;</button><span class="list">' + elementsOfLines + '</span><button class="delete">X</button></li>');
+	   $('#itemList').append(item);
+
+	});
 }
-.my-simple-gallery img {
-  width: 100%;
-  height: auto;
+
+function updateStorage() {
+    // Dumping current storage
+    localStorage.setItem("toBuy","");
+    localStorage.setItem("bought","");
+
+    var toBuyList = [];
+    var boughtList = [];
+    // for each of list
+    $( ".item" ).each(function() {
+        var text = $( this ).children( ".list" ).text();
+        // Sorting them out
+        if( $( this ).hasClass( "bought" ).toString() == "true" ){
+            boughtList.push(text);
+        }
+        else{
+            toBuyList.push(text);
+        }
+    });
+
+    localStorage.setItem("toBuy", JSON.stringify(toBuyList));
+    localStorage.setItem("bought", JSON.stringify(boughtList));  
 }
-.my-simple-gallery figure {
-  display: block;
-  float: left;
-  margin: 0 5px 5px 0;
-  width: 150px;
+
+function addListItem() {
+	
+	var write = $('#newItem').val();
+	var list = $('#itemList');
+	var item = $('<li class="item"><button class="checkbox">&#x2713;</button><span class="list">' + write + '</span><button class="delete">X</button></li>');
+
+	if (write.length === 0  || write.length > 33) {
+	   return false;
+	}
+	
+	list.prepend(item);
+	$("#newItem").val('');
+	$("#newItem").focus();
+
+    updateStorage();
 }
-.my-simple-gallery figcaption {
-  display: none;
-}</style></head>
-<body>
-    <div class="nav">
-        <ul>
-            <li class="letaky active"><a href="letaky.html">Letáky</a></li>
-            <li class="star"><a href="#"><img src="http://192.168.1.101/c/star.png" /></a></li>
-            <li class="seznam"><a href="index.html">Seznam</a></li>
-        </ul>
-    </div>
-<h2>First gallery:</h2>
 
-  <div class="my-simple-gallery" itemscope itemtype="http://schema.org/ImageGallery">
+function deleteItem() {
 
-    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-      <a href="https://farm3.staticflickr.com/2567/5697107145_a4c2eaa0cd_o.jpg" itemprop="contentUrl" data-size="1024x1024">
-          <img src="https://farm3.staticflickr.com/2567/5697107145_3c27ff3cd1_m.jpg" itemprop="thumbnail" alt="Image description" />
-      </a>
-                                          <figcaption itemprop="caption description">Image caption  1</figcaption>
-                                          
-    </figure>
+	$(this).parent().remove();
 
-    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-      <a href="https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg" itemprop="contentUrl" data-size="964x1024">
-          <img src="https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_m.jpg" itemprop="thumbnail" alt="Image description" />
-      </a>
-      <figcaption itemprop="caption description">Image caption 2</figcaption>
-    </figure>
+    updateStorage();
+}
 
-    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-      <a href="https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg" itemprop="contentUrl" data-size="1024x683">
-          <img src="https://farm7.staticflickr.com/6175/6176698785_7dee72237e_m.jpg" itemprop="thumbnail" alt="Image description" />
-      </a>
-      <figcaption itemprop="caption description">Image caption 3</figcaption>
-    </figure>
+function toggleItem() {
 
-    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-      <a href="https://farm6.staticflickr.com/5023/5578283926_822e5e5791_b.jpg" itemprop="contentUrl" data-size="1024x768">
-          <img src="https://farm6.staticflickr.com/5023/5578283926_822e5e5791_m.jpg" itemprop="thumbnail" alt="Image description" />
-      </a>
-      <figcaption itemprop="caption description">Image caption 4</figcaption>
-    </figure>
+	$(this).parent().toggleClass('bought');
+	$(this).parent().appendTo('ul#itemList');
 
+    updateStorage();
 
-  </div>
+    // List is redrawn to put toBuy item on top
+    $('#itemList').html("");
+    init();
+}
 
+$(function() {
 
-
-
-
-<!-- Root element of PhotoSwipe. Must have class pswp. -->
-<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
-
-    <!-- Background of PhotoSwipe. 
-         It's a separate element, as animating opacity is faster than rgba(). -->
-    <div class="pswp__bg"></div>
-
-    <!-- Slides wrapper with overflow:hidden. -->
-    <div class="pswp__scroll-wrap">
-
-        <!-- Container that holds slides. PhotoSwipe keeps only 3 slides in DOM to save memory. -->
-        <!-- don't modify these 3 pswp__item elements, data is added later on. -->
-        <div class="pswp__container">
-            <div class="pswp__item"></div>
-            <div class="pswp__item"></div>
-            <div class="pswp__item"></div>
-        </div>
-
-        <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
-        <div class="pswp__ui pswp__ui--hidden">
-
-            <div class="pswp__top-bar">
-
-                <!--  Controls are self-explanatory. Order can be changed. -->
-
-                <div class="pswp__counter"></div>
-
-                <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>  
-
-                <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
-
-                <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
-
-                <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
-                <!-- element will get class pswp__preloader--active when preloader is running -->
-                <div class="pswp__preloader">
-                    <div class="pswp__preloader__icn">
-                      <div class="pswp__preloader__cut">
-                        <div class="pswp__preloader__donut"></div>
-                      </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-                <div class="pswp__share-tooltip"></div> 
-            </div>
-
-            <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
-            </button>
-
-            <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
-            </button>
-
-            <div class="pswp__caption">
-                <div class="pswp__caption__center"></div>
-            </div>
-
-          </div>
-
-        </div>
-
-</div>
-<script src='http://192.168.1.101/c/assets/common/stopExecutionOnTimeout.js?t=1'></script>
-<script src='http://photoswipe.s3-eu-west-1.amazonaws.com/pswp/dist/photoswipe.min.js'></script>
-<script src='http://photoswipe.s3-eu-west-1.amazonaws.com/pswp/dist/photoswipe-ui-default.min.js'></script>
-<script>
-var initPhotoSwipeFromDOM = function (gallerySelector) {
-    var parseThumbnailElements = function (el) {
-        var thumbElements = el.childNodes, numNodes = thumbElements.length, items = [], figureEl, childElements, linkEl, size, item;
-        for (var i = 0; i < numNodes; i++) {
-            if (window.CP.shouldStopExecution(1)) {
-                break;
-            }
-            figureEl = thumbElements[i];
-            if (figureEl.nodeType !== 1) {
-                continue;
-            }
-            linkEl = figureEl.children[0];
-            size = linkEl.getAttribute('data-size').split('x');
-            item = {
-                src: linkEl.getAttribute('href'),
-                w: parseInt(size[0], 10),
-                h: parseInt(size[1], 10)
-            };
-            if (figureEl.children.length > 1) {
-                item.title = figureEl.children[1].innerHTML;
-            }
-            if (linkEl.children.length > 0) {
-                item.msrc = linkEl.children[0].getAttribute('src');
-            }
-            item.el = figureEl;
-            items.push(item);
-        }
-        window.CP.exitedLoop(1);
-        return items;
-    };
-    var closest = function closest(el, fn) {
-        return el && (fn(el) ? el : closest(el.parentNode, fn));
-    };
-    var onThumbnailsClick = function (e) {
-        e = e || window.event;
-        e.preventDefault ? e.preventDefault() : e.returnValue = false;
-        var eTarget = e.target || e.srcElement;
-        var clickedListItem = closest(eTarget, function (el) {
-            return el.tagName && el.tagName.toUpperCase() === 'FIGURE';
-        });
-        if (!clickedListItem) {
-            return;
-        }
-        var clickedGallery = clickedListItem.parentNode, childNodes = clickedListItem.parentNode.childNodes, numChildNodes = childNodes.length, nodeIndex = 0, index;
-        for (var i = 0; i < numChildNodes; i++) {
-            if (window.CP.shouldStopExecution(2)) {
-                break;
-            }
-            if (childNodes[i].nodeType !== 1) {
-                continue;
-            }
-            if (childNodes[i] === clickedListItem) {
-                index = nodeIndex;
-                break;
-            }
-            nodeIndex++;
-        }
-        window.CP.exitedLoop(2);
-        if (index >= 0) {
-            openPhotoSwipe(index, clickedGallery);
-        }
-        return false;
-    };
-    var photoswipeParseHash = function () {
-        var hash = window.location.hash.substring(1), params = {};
-        if (hash.length < 5) {
-            return params;
-        }
-        var vars = hash.split('&');
-        for (var i = 0; i < vars.length; i++) {
-            if (window.CP.shouldStopExecution(3)) {
-                break;
-            }
-            if (!vars[i]) {
-                continue;
-            }
-            var pair = vars[i].split('=');
-            if (pair.length < 2) {
-                continue;
-            }
-            params[pair[0]] = pair[1];
-        }
-        window.CP.exitedLoop(3);
-        if (params.gid) {
-            params.gid = parseInt(params.gid, 10);
-        }
-        if (!params.hasOwnProperty('pid')) {
-            return params;
-        }
-        params.pid = parseInt(params.pid, 10);
-        return params;
-    };
-    var openPhotoSwipe = function (index, galleryElement, disableAnimation) {
-        var pswpElement = document.querySelectorAll('.pswp')[0], gallery, options, items;
-        items = parseThumbnailElements(galleryElement);
-        options = {
-            index: index,
-            galleryUID: galleryElement.getAttribute('data-pswp-uid'),
-            getThumbBoundsFn: function (index) {
-                var thumbnail = items[index].el.getElementsByTagName('img')[0], pageYScroll = window.pageYOffset || document.documentElement.scrollTop, rect = thumbnail.getBoundingClientRect();
-                return {
-                    x: rect.left,
-                    y: rect.top + pageYScroll,
-                    w: rect.width
-                };
-            },
-            history: false,
-            focus: false
-        };
-        if (disableAnimation) {
-            options.showAnimationDuration = 0;
-        }
-        gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
-        gallery.init();
-    };
-    var galleryElements = document.querySelectorAll(gallerySelector);
-    for (var i = 0, l = galleryElements.length; i < l; i++) {
-        if (window.CP.shouldStopExecution(4)) {
-            break;
-        }
-        galleryElements[i].setAttribute('data-pswp-uid', i + 1);
-        galleryElements[i].onclick = onThumbnailsClick;
-    }
-    window.CP.exitedLoop(4);
-    var hashData = photoswipeParseHash();
-    if (hashData.pid > 0 && hashData.gid > 0) {
-        openPhotoSwipe(hashData.pid - 1, galleryElements[hashData.gid - 1], true);
-    }
-};
-initPhotoSwipeFromDOM('.my-simple-gallery');
-</script>
-<script src='http://codepen.io/assets/editor/live/css_live_reload_init.js'></script>
-</body></html>
+	var add = $('#addItem');
+	var newItem = $('#newItem');
+	var list = $('#itemList');
+	
+	add.on('click', addListItem);
+	list.on('click', '.checkbox', toggleItem);
+	list.on('click', '.delete', deleteItem);
+	newItem.on('keypress', function (e) {
+	   if (e.which == 13) {
+		  addListItem();
+	   }
+	});
+});
