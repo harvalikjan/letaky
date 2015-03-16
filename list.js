99f1d387be3565f6620c0b696bcb05f7bc1447e9
harvalikjan <harvalikjan@gmail.com>
2015-03-16 17:37:32 +0100

    Create list.js

---
+++ list.js
@@ -0,0 +1,107 @@
+function init() {
+    // toBuy list 
+	// Parse local storage toBuy list to array 
+	var toBuy = JSON.parse(localStorage["toBuy"]);
+
+	// for each of toBuy list   
+	toBuy.forEach(function(listEntries) {
+
+	   var stringifiedLine = JSON.stringify(listEntries);
+	   var elementsOfLines = JSON.parse(stringifiedLine);
+
+	   // line from addListItem()
+	   item = $('<li class="item"><button class="checkbox">&#x2713;</button><span class="list">' + elementsOfLines + '</span><button class="delete">X</button></li>');
+	   $('#itemList').append(item);   
+	});
+
+    // Bought list 
+	// Parse local storage bought list to array 
+	var bought = JSON.parse(localStorage["bought"]);
+
+	// for each of bought list   
+	bought.forEach(function(listEntries) {
+
+	   var stringifiedLine = JSON.stringify(listEntries);
+	   var elementsOfLines = JSON.parse(stringifiedLine);
+	   
+	   // line from addListItem()
+	   item = $('<li class="item bought"><button class="checkbox">&#x2713;</button><span class="list">' + elementsOfLines + '</span><button class="delete">X</button></li>');
+	   $('#itemList').append(item);
+
+	});
+}
+
+function updateStorage() {
+    // Dumping current storage
+    localStorage.setItem("toBuy","");
+    localStorage.setItem("bought","");
+
+    var toBuyList = [];
+    var boughtList = [];
+    // for each of list
+    $( ".item" ).each(function() {
+        var text = $( this ).children( ".list" ).text();
+        // Sorting them out
+        if( $( this ).hasClass( "bought" ).toString() == "true" ){
+            boughtList.push(text);
+        }
+        else{
+            toBuyList.push(text);
+        }
+    });
+
+    localStorage.setItem("toBuy", JSON.stringify(toBuyList));
+    localStorage.setItem("bought", JSON.stringify(boughtList));  
+}
+
+function addListItem() {
+	
+	var write = $('#newItem').val();
+	var list = $('#itemList');
+	var item = $('<li class="item"><button class="checkbox">&#x2713;</button><span class="list">' + write + '</span><button class="delete">X</button></li>');
+
+	if (write.length === 0  || write.length > 33) {
+	   return false;
+	}
+	
+	list.prepend(item);
+	$("#newItem").val('');
+	$("#newItem").focus();
+
+    updateStorage();
+}
+
+function deleteItem() {
+
+	$(this).parent().remove();
+
+    updateStorage();
+}
+
+function toggleItem() {
+
+	$(this).parent().toggleClass('bought');
+	$(this).parent().appendTo('ul#itemList');
+
+    updateStorage();
+
+    // List is redrawn to put toBuy item on top
+    $('#itemList').html("");
+    init();
+}
+
+$(function() {
+
+	var add = $('#addItem');
+	var newItem = $('#newItem');
+	var list = $('#itemList');
+	
+	add.on('click', addListItem);
+	list.on('click', '.checkbox', toggleItem);
+	list.on('click', '.delete', deleteItem);
+	newItem.on('keypress', function (e) {
+	   if (e.which == 13) {
+		  addListItem();
+	   }
+	});
+});
